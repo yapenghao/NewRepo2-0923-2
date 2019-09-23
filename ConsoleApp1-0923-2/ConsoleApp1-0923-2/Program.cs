06ccd93019beb34c48f608ed0bd9994fa8c35ae0
yapeng hao <yapeng.hao@email.com>
2019-09-23 13:07:42 -0400

    for commit

---
+++ ConsoleApp1-0923-2/ConsoleApp1-0923-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1_0923_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Thread.Sleep("3000);
+
+        }
+    }
+}
